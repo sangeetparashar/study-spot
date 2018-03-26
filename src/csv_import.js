a224e53ba71ed3403354bcca3c407e44f2ebddb7
Sangeet Parashar <sparasha@uwo.ca>
2018-03-26 16:43:41 -0400

    formatting data to be imported into MongoDB

--- src/csv_import.js
+++ src/csv_import.js
@@ -1,6 +1,6 @@
 var MongoClient = require('mongodb').MongoClient;
 var async = require('async');
-var Course = require('/db/Course');
+var Course = require('/models/Course');
 
 
 
@@ -12,12 +12,19 @@ var Course = require('/db/Course');
     var collection = db.collection('Courses');
     async.every(csv, function(row, callback) {
 
-       // build the csv row into an object literal
-    //    var data = { Course.code: CourseCode }; // make this fit your data correctly
-       collection.insert(data, function(err, docs) {
-          // signal that this row is done inserting
-          // by calling the callback passed with every row
-          callback(err);
+        let newCourse = new Course;
+        
+        // build the csv row into an object literal
+        newCourse = { 
+          code:  "jack me off",
+          start: "boner"
+        }; 
+        
+        // make this fit your data correctly
+        collection.insert(newCourse, function(err, docs) {
+            // signal that this row is done inserting
+            // by calling the callback passed with every row
+            callback(err);
        });
     }, function() {
         // all rows have been inserted, so close the db connection
