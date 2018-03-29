948e5bc7f4dc25c61932b0234813a478919ef666
Sangeet Parashar <sparasha@uwo.ca>
2018-03-29 11:54:48 -0400

    creating an array of the JSON objects

--- src/formatJSON.js
+++ src/formatJSON.js
@@ -1,34 +1,23 @@
 const { ObjectID } = require('mongodb');
 const { mongoose } = require('./db/db');
 const MongoClient = require('mongodb').MongoClient;
-var express = require('express');
-var app = express();
+
 var path = require('path');
 var engines = require('consolidate');
 var bodyParser = require('body-parser');
 var assert = require('assert');
-
 const jsonObject = require('../scraper/data.json')
 const Course = require('./models/Course');
 
 let course = {};
 let day = [];
-var db;
-
-MongoClient.connect('mongodb://admin:admin@ds155278.mlab.com:55278/4471',
-                     function(err, database) {
-    assert.equal(null, err);
-    console.log("Successfully connected to MongoDB.");
-    const db = database.db('CS4471');
+let arrayOfJson = []
 
 
-    
 
-
-    app.get('/', function(req, res, next) {
         for(var j = 0; j<=Object.keys(jsonObject).length; j++){
-            console.log(Object.keys(jsonObject).length);
-            console.log(j);
+            // console.log(Object.keys(jsonObject).length);
+            // console.log(j);
             let subjects = jsonObject[j];
             for (let classes in subjects){
             
@@ -41,7 +30,6 @@ MongoClient.connect('mongodb://admin:admin@ds155278.mlab.com:55278/4471',
                     for (var i =0; i<lastArray.length; i++){
             
                         if(i%12 === 0){
-                            console.log('---------------------------')
                             course = {};
                             day = [];
                         }
@@ -59,27 +47,16 @@ MongoClient.connect('mongodb://admin:admin@ds155278.mlab.com:55278/4471',
                         if(i%12 === 11){
                             course.code = classes;
                             course.location = lastArray[i];
-                            db.collection('Courses').insertOne(
-                                {
-                                    'code': course.code, 'start': course.start, 'end': course.end, 'location':course.location, 'days': course.days
-                                },
-                                function(err, response){
-                                    console.log('is this null?')
-                                }
-                            );
-                            console.log(course);
-
+                            arrayOfJson.push(course);
                         }
                             
                     }
                 }
     
             }
-        }
+        };
 
-    });
-    app.listen(3000, () => {
-        console.log( 'listening on localhost:3000');
-    });
-});
+
+console.log(arrayOfJson);
+
 
